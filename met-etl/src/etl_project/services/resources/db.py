922335769a81e0cd7b5ef85084581d3dbcbbd45f
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2022-11-06 18:24:00 -0800

    Testing second db

--- met-etl/src/etl_project/services/resources/db.py
+++ met-etl/src/etl_project/services/resources/db.py
@@ -21,3 +21,21 @@ def met_db_session(context):
     finally:
         context.log.info('Close connection here')
         ## TODO handle session closing
+
+
+@resource
+@contextmanager
+def met_etl_db_session(context):
+    try:
+        user = os.getenv("MET_ANALYTICS_DB_USER", "")
+        password = os.getenv("MET_ANALYTICS_DB_PASSWORD", "")
+        db = os.getenv("MET_ANALYTICS_DB_DB", "")
+        host = os.getenv("MET_ANALYTICS_DB_HOST", "")
+        port = os.getenv("MET_ANALYTICS_DB_PORT", 54334)
+        db_connection = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+        session_maker = sessionmaker(bind=db_connection)
+        session = session_maker()
+        yield session
+    finally:
+        context.log.info('Close connection here')
+        ## TODO handle session closing
